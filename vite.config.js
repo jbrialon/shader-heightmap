6f1e5d53b5fb1b2fdc9c3d2fcd3b08a2a13a361c
Jérémy Brialon <jeremy.brialon@merkle.com>
2023-02-28 16:48:04 +0100

    update build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
-
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
-}
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true,
+    open: true,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    target: "esnext",
+  },
+};
